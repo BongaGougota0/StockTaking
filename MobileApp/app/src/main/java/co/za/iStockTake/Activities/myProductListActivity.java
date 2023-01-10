a59c77f305e867f9294e65ed39962b3943b1b532
BongaGougota0 <bonga_gougota@outlook.com>
2023-01-10 19:48:49 +0200

    write do API page function. json object visualized

--- MobileApp/app/src/main/java/co/za/iStockTake/Activities/myProductListActivity.java
+++ MobileApp/app/src/main/java/co/za/iStockTake/Activities/myProductListActivity.java
@@ -3,6 +3,8 @@ package co.za.iStockTake.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.textclassifier.TextLinks;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -15,11 +17,20 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 import co.za.iStockTake.Adapters.ProductListAdapter;
+import co.za.iStockTake.Constant;
 import co.za.iStockTake.Interfaces.ChangeNumberItemsListener;
 import co.za.iStockTake.ManagementList;
+import co.za.iStockTake.Models.Product;
 import co.za.iStockTake.R;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class myProductListActivity extends AppCompatActivity
 {
@@ -39,6 +50,7 @@ public class myProductListActivity extends AppCompatActivity
     RecyclerView recyclerView;
     TextView txtTax, txtTotal;
     ManagementList managementList;
+    TextView savelist;
 
     ImageView home_, logout_, specials_, trending_;
     @Override
@@ -54,10 +66,62 @@ public class myProductListActivity extends AppCompatActivity
         txtTotal = findViewById(R.id.total);
         scrollView = findViewById(R.id.listScrollView);
         recyclerView = findViewById(R.id.listProducts);
+        savelist = findViewById(R.id.saveList);
 
         initlist();
         calculateListCost();
         bottomNavigation();
+
+        savelist.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                setSavelist();
+            }
+        });
+    }
+
+    public void setSavelist()
+    {
+        okhttp3.OkHttpClient client = new okhttp3.OkHttpClient();
+        String json = new Gson().toJson(managementList.getList());
+        RequestBody body = RequestBody.create(Constant.JSON,
+                new Gson().toJson(new DataContainer("10", managementList.getList())));
+        //check
+        System.out.println("###Data to send to API"+json);
+        Request request = new Request.Builder()
+                .url("http://localhost:5000/CreateList")
+                .post(body)
+                .build();
+
+        try
+        {
+            Response response = client.newCall(request).execute();
+            if(response.isSuccessful())
+            {
+
+            }else
+            {
+
+            }
+
+        }catch (IOException ioException)
+        {
+
+        }
+    }
+
+    static class DataContainer
+    {
+        String user_id;
+        ArrayList<Product> products;
+
+        public DataContainer(String user_id, ArrayList<Product> products)
+        {
+            this.user_id = user_id;
+            this.products = products;
+        }
     }
 
     public void initlist()
--- MobileApp/app/src/main/java/co/za/iStockTake/Constant.java
+++ MobileApp/app/src/main/java/co/za/iStockTake/Constant.java
@@ -1,8 +1,11 @@
 package co.za.iStockTake;
 
+import okhttp3.MediaType;
+
 public class Constant
 {
     //params
     public static String user_id;
     public static String product_id;
+    public static final MediaType JSON = MediaType.parse("application/json;charset=utf-8");
 }
