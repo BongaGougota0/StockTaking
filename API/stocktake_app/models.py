24b3905700dd34f26a09b08c53844d363ff7d4b5
BongaGougota0 <bonga_gougota@outlook.com>
2023-01-14 13:42:51 +0200

    add models (product, category, store, admin_user)

--- API/stocktake_app/models.py
+++ API/stocktake_app/models.py
@@ -21,8 +21,7 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-
+        
 class List(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     items = db.Column(db.String(600), nullable=False) #should be an array of items
@@ -31,3 +30,70 @@ class List(db.Model):
 
     def __repr__(self):
         return f"List('{self.items}', '{self.date_created}')"
+
+class StoreAdmin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String(20), unique=True, nullable=False)
+
+    #relationship between user and the lists they create
+    stores = db.relationship('Store', backref='creator', lazy=True)
+
+    #Categories created
+    cate = db.relationship('Category', backref='create_cat', lazy=True)
+
+    #hashing algorithm used 60 chars
+    image_file = db.Column(db.String(60), nullable=False, default='default.jpg')
+
+    def __repr__(self):
+        return f"StoreAdmin('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Store(db.Model):
+    store_id = db.Column(db.Integer(), primary_key=True)
+    store_name = db.Column(db.String(30), unique=True, nullable=False)
+    store_location = db.Column(db.String(50), nullable=False)
+    store_description = db.Column(db.String(100))
+    store_email = db.Column(db.String(60), nullable=False)
+    store_contact = db.Column(db.String(60), nullable=False)
+    store_logo = db.Column(db.String(60), nullable=False, default='default_store_logo.jpg')
+
+    user_id = db.Column(db.Integer, db.ForeignKey('storeadmin.id'), nullable=False)
+    #each store has many products 1-to-M
+    products = db.relationship('Product', backref='store_prod', lazy=True)
+
+    def __repr__(self):
+        return f"Store('{self.store_name}', '{self.store_location}', '{self.store_description}','{self.store_email}','{self.store_contact}', '{self.store_logo}')"
+
+class Product(db.Model):
+    product_id = db.Column(db.Integer(), primary_key=True)
+    product_name = db.Column(db.String(30), unique=True, nullable=False)
+    product_description = db.Column(db.String(100))
+    product_category = db.Column(db.String(30), nullable=False)
+    product_price = db.Column(db.Integer(), nullable=False)
+    img_product = db.Column(db.String(60), nullable=False, default='default_product.jpg')
+
+    #each product is created for a specific store
+    # one store --> Many products relationship M-to-1
+    creator = db.Column(db.Integer, db.ForeignKey('store.store_id'), nullable=False)
+
+    #each product falls under a category
+
+    def __repr__(self):
+        return f"Product('{self.product_name}', '{self.product_price}','{self.product_category}', '{self.product_description}', '{self.img_product}')"
+
+
+class Category(db.Model):
+    category_id = db.Column(db.Integer(), primary_key=True)
+    category_name = db.Column(db.String(30), unique=True, nullable=False)
+    category_description = db.Column(db.String(30))
+    img_category_product = db.Column(db.String(60), nullable=False, default='default_icon.jpg')
+
+    #A category can be created by any admin, and should be accessible by any other admin
+    # one store --> Many products relationship 1-to-M
+    creator = db.Column(db.Integer, db.ForeignKey('storeadmin.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Category('{self.category_name}', '{self.category_description}', '{self.img_category_product}')"
+
+          
